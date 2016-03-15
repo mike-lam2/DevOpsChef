9904b520e6ac33abd8a86f9020b4f208d3a49c4a
mike-lam2 <author@example.com>
2016-03-15 00:14:41 -0400

    .

--- chefcli.sh
+++ chefcli.sh
@@ -2,5 +2,5 @@ cd /home/ec2-user/DevOpsChef
 git pull --no-commit
 cp -R /home/ec2-user/DevOpsChef/chef-repo/* /var/chef/chef-repo
 cd /var/chef/chef-repo/cookbooks
-chef-client --local-mode --runlist recipe[infrastructure::ec2_cli] >> /var/log/chef/chef-client.log
+chef-client --local-mode --runlist recipe[infrastructure::ec2_cli] >> /var/log/chef-client.log
 cd /home/ec2-user/DevOpsChef
