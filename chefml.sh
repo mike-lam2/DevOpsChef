06948822aed45a621ce77e44f69969bd36b04de0
mike-lam2 <author@example.com>
2016-03-15 00:02:26 -0400

    .

--- chefml.sh
+++ chefml.sh
@@ -1,4 +1,6 @@
+cd /home/ec2-user/DevOpsChef
+git pull --no-commit
 cp -R /home/ec2-user/DevOpsChef/chef-repo/* /var/chef/chef-repo
 cd /var/chef/chef-repo/cookbooks
-chef-client --local-mode --runlist recipe[$1]
+chef-client --local-mode --runlist recipe[$1] >> /var/log/chef/chef-client.log
 cd /home/ec2-user/DevOpsChef
--- chefwebapp.sh
+++ chefwebapp.sh
@@ -1,4 +1,6 @@
+cd /home/ec2-user/DevOpsChef
+git pull --no-commit
 cp -R /home/ec2-user/DevOpsChef/chef-repo/* /var/chef/chef-repo
 cd /var/chef/chef-repo/cookbooks
-chef-client --local-mode --runlist recipe[infrastructure::ec2_webapp]
+chef-client --local-mode --runlist recipe[infrastructure::ec2_webapp] >> /var/log/chef/chef-client.log
 cd /home/ec2-user/DevOpsChef
