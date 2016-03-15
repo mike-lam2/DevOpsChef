58c9b51166e70189a398898b7c920eff7e2a2ace
mike-lam2 <author@example.com>
2016-03-15 18:30:45 -0400

    .

--- chef-repo/cookbooks/webapp/attributes/default.rb
+++ chef-repo/cookbooks/webapp/attributes/default.rb
@@ -1,4 +1,4 @@
  default['webapp']['Application'] = node['ec2']['Application']
  default['webapp']['BranchToClone'] = node['ec2']['BranchToClone']
- default['webapp']['Gitsite'] = node['ec2']['https://github.com/mike-lam2/']
+ default['webapp']['Gitsite'] = node['ec2']['Gitsite']
  
