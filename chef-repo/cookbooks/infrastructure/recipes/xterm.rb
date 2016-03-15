ad4bf0a40e8cdfc9ab3b42b3c3c3306367b7ab3e
mike-lam2 <author@example.com>
2016-03-14 22:40:02 -0400

    .

--- chef-repo/cookbooks/infrastructure/recipes/xterm.rb
+++ chef-repo/cookbooks/infrastructure/recipes/xterm.rb
@@ -1,5 +1,7 @@
 StackName = node.default['default']['StackName']
-label='echo -ne "\033]0;${USER}@'+StackName+':${PWD/#$HOME/~}\007"'
+#label='echo -ne "\033]0;${USER}@'+StackName+':${PWD/#$HOME/~}\007"'
+label='echo -ne a'+StackName+'z'
+
 file '/etc/sysconfig/bash-prompt-xterm' do
   action :create
   mode '0555'
