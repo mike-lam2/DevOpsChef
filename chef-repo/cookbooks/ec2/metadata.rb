746426836c3e09501b34b8063efa0fbac3112da9
mike-lam2 <mike-lam2@example.com>
2016-03-16 08:57:25 -0400

    added comments

--- chef-repo/cookbooks/ec2/metadata.rb
+++ chef-repo/cookbooks/ec2/metadata.rb
@@ -1,1 +1,3 @@
 name 'ec2'
+description 'based ec2'
+long_description  IO.read(File.join(File.dirname(__FILE__),'README.rdoc'))
--- chef-repo/cookbooks/webapp/metadata.rb
+++ chef-repo/cookbooks/webapp/metadata.rb
@@ -1,3 +1,4 @@
 name 'webapp'
 description 'based on concepts from http://stackoverflow.com/questions/33266015/chef-use-attribute-in-different-cookbook'
+long_description  IO.read(File.join(File.dirname(__FILE__),'README.rdoc'))
 depends 'ec2'
