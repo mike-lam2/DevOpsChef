e8f07c48b8e261e7edb8c4f2cfcd27ea4dfa4274
mike-lam2 <mike-lam2@example.com>
2016-03-16 14:51:16 -0400

    .

---
+++ chef-repo/cookbooks/learn-jenkings/metadata.rb
@@ -0,0 +1,12 @@
+name 'jenkins-install'
+maintainer 'John Mueller'
+maintainer_email 'john.mueller.ottawa@gmail.com'
+license 'all_rights'
+description 'Installs/Configures jenkins-install'
+long_description 'Installs/Configures jenkins-install'
+version '0.1.0'
+
+depends 'apt', '~> 3.0.0'
+depends 'firewall', '~> 2.5.0'
+depends 'jenkins', '~> 3.0.0'
+
