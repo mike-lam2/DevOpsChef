4f984b3d7ec59776d39357c70c87197de16ab1f8
mike-lam2 <author@example.com>
2016-03-17 14:03:13 -0400

    .

--- chef-repo/cookbooks/webserver/attributes/default.rb
+++ chef-repo/cookbooks/webserver/attributes/default.rb
@@ -1,2 +1,2 @@
-default['webserver']['WebserverType'] = node['ec2']['WebserverType']
-default['webserver']['WebserverBase'] = '/var/www/html or $JETTY_BASE will be set in recipe'
+#default['webserver']['WebserverType'] = node['ec2']['WebserverType']
+#default['webserver']['WebserverBase'] = '/var/www/html or $JETTY_BASE will be set in recipe'
