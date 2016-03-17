d2467bebd6b305756ec2cccc92556ce39eaca306
mike-lam2 <author@example.com>
2016-03-17 13:05:30 -0400

    jetty.rb

---
+++ chef-repo/cookbooks/webserver/recipes/jetty.rb
@@ -0,0 +1,10 @@
+bash 'jetty' do
+  cwd '/home/ec2-user/'
+  code <<-EOH
+wget http://download.eclipse.org/jetty/9.2.15.v20160210/dist/jetty-distribution-9.2.15.v20160210.tar.gz
+tar xvf jetty-distribution-9.2.15.v20160210.tar.gz
+cd jetty-distribution-9.2.15.v20160210
+cd demo-base
+java -jar ../start.jar
+    EOH
+end
