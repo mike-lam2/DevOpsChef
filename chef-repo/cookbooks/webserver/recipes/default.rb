270ed39065eb04ced37779a17b847fab603a105d
mike-lam2 <author@example.com>
2016-03-17 12:37:21 -0400

    .

--- chef-repo/cookbooks/webserver/recipes/default.rb
+++ chef-repo/cookbooks/webserver/recipes/default.rb
@@ -2,11 +2,11 @@ include_recipe 'ec2'
 
 if "httpd".eql?(node.default['webserver']['WebserverType'])
   include_recipe 'webserver::httpd'
-elseif if "jetty".eql?(node.default['webserver']['WebserverType'])
+elseif "jetty".eql?(node.default['webserver']['WebserverType'])
   include_recipe 'webserver::jetty'
 else
   log 'message' do
-    message 'ERROR UNKNOWNED SERVER TO INSTALL.'
+    message 'ERROR UNKNOWNED SERVER TO INSTALL:'+node.default['webserver']['WebserverType']
     level :error
   end
 end
