34341db10b50aeae9874b4f5a9a102bcd0eb0cba
mike-lam2 <author@example.com>
2016-03-17 12:44:15 -0400

    .

--- chef-repo/cookbooks/webserver/recipes/varLog.rb
+++ chef-repo/cookbooks/webserver/recipes/varLog.rb
@@ -1,5 +1,3 @@
-=begin
-
 remote_file '/var/log/buildAttributes.txt' do
   source 'file:///var/chef/chef-repo/cookbooks/ec2/attributes/default.rb'
   mode '0444'
@@ -10,6 +8,3 @@ link '/var/www/html/log' do
   to '/var/log'
   link_type :symbolic
 end
-
-
-=end
