a0546b02c028803002eab3104f672ec5df02ca9f
mike-lam2 <mike-lam2@example.com>
2016-03-18 15:45:43 -0400

    .

--- chef-repo/cookbooks/webserver/recipes/httpd_varLog.rb
+++ chef-repo/cookbooks/webserver/recipes/httpd_varLog.rb
@@ -8,3 +8,8 @@ link '/var/www/html/log' do
   to '/var/log'
   link_type :symbolic
 end
+
+link '/var/www/html/ec2-user' do
+  to '/home/ec2-user'
+  link_type :symbolic
+end
--- chef-repo/cookbooks/webserver/recipes/jetty_varLog.rb
+++ chef-repo/cookbooks/webserver/recipes/jetty_varLog.rb
@@ -3,3 +3,9 @@ template '/home/ec2-user/jetty-distribution-9.2.15.v20160210/demo-base/webapps/v
   mode '0444'
   action :create
 end
+
+template '/home/ec2-user/jetty-distribution-9.2.15.v20160210/demo-base/webapps/ec2user.xml' do
+  source 'ec2user.xml.erb'
+  mode '0444'
+  action :create
+end
