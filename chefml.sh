1c81869b2203ad30bb609d6bdbdf4adc62b83b99
mike-lam2 <mike-lam2@example.com>
2016-03-18 14:44:57 -0400

    jenkins1

--- chefml.sh
+++
@@ -1,6 +0,0 @@
-cd /home/ec2-user/DevOpsChef
-git pull --no-commit
-cp -R /home/ec2-user/DevOpsChef/chef-repo/* /var/chef/chef-repo
-cd /var/chef/chef-repo/cookbooks
-chef-client --local-mode --runlist recipe[$1] >> /var/log/chef-client.log
-cd /home/ec2-user/DevOpsChef
