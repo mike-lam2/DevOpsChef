2ee218e554a3261577781901cf287c11cbee3b39
mike-lam2 <author@example.com>
2016-03-19 12:15:31 -0400

    .

--- chef-repo/cookbooks/webserver/recipes/jetty.rb
+++ chef-repo/cookbooks/webserver/recipes/jetty.rb
@@ -3,9 +3,30 @@ bash 'jetty' do
   code <<-EOH
 wget http://download.eclipse.org/jetty/9.2.15.v20160210/dist/jetty-distribution-9.2.15.v20160210.tar.gz
 tar xvf jetty-distribution-9.2.15.v20160210.tar.gz
-cd jetty-distribution-9.2.15.v20160210
-cd demo-base
-java -jar /home/ec2-user/jetty-distribution-9.2.15.v20160210/start.jar &
+mv jetty-distribution-9.2.15.v20160210 jetty_home
+chown -R ec2-user:ec2-user jetty_home
+mv jetty_home/demo-base jetty_base
+    EOH
+end
+
+template '/home/ec2-user/jetty_start.sh' do
+  source 'jetty_start.sh.erb'
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0700'
+end
+
+template '/home/ec2-user/jetty_stop.sh' do
+  source 'jetty_stop.sh.erb'
+  owner 'ec2-user'
+  group 'ec2-user'
+  mode '0700'
+end
+
+=begin
+THIS PIECE DOES NOT WORK
+cd jetty_base
+java -jar /home/ec2-user/jetty_home/start.jar &
     EOH
 end
 
@@ -16,5 +37,5 @@ template '/etc/init.d/sysvScriptJetty ' do
   group 'root'
   mode '0444'
 end
-
+=end
 
--- chef-repo/cookbooks/webserver/recipes/jetty_varLog.rb
+++ chef-repo/cookbooks/webserver/recipes/jetty_varLog.rb
@@ -1,10 +1,10 @@
-template '/home/ec2-user/jetty-distribution-9.2.15.v20160210/demo-base/webapps/varlog.xml' do
+template '/home/ec2-user/jetty_base/webapps/varlog.xml' do
   source 'varlog.xml.erb'
   mode '0444'
   action :create
 end
 
-template '/home/ec2-user/jetty-distribution-9.2.15.v20160210/demo-base/webapps/ec2user.xml' do
+template '/home/ec2-user/jetty_base/webapps/ec2user.xml' do
   source 'ec2user.xml.erb'
   mode '0444'
   action :create
