72645493a6c3c1544ccdcee5f635bf9acf87d467
mike-lam2 <author@example.com>
2016-03-19 13:25:46 -0400

    .

--- chef-repo/cookbooks/webserver/recipes/jetty.rb
+++ chef-repo/cookbooks/webserver/recipes/jetty.rb
@@ -3,6 +3,7 @@ bash 'jetty' do
   code <<-EOH
 wget http://download.eclipse.org/jetty/9.2.15.v20160210/dist/jetty-distribution-9.2.15.v20160210.tar.gz
 tar xvf jetty-distribution-9.2.15.v20160210.tar.gz
+rm jetty-distribution-9.2.15.v20160210.tar.gz
 mv jetty-distribution-9.2.15.v20160210 jetty_home
 chown -R ec2-user:ec2-user jetty_home
 mv jetty_home/demo-base jetty_base
